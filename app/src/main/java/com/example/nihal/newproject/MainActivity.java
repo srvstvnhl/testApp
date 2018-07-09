197d3ddc69662cc7f661ad9e9ed50e6450defd33
srvstvnhl <srvstvnhl@gmail.com>
2018-07-09 17:45:18 +0530

    third commit

--- app/src/main/java/com/example/nihal/newproject/MainActivity.java
+++ app/src/main/java/com/example/nihal/newproject/MainActivity.java
@@ -20,22 +20,23 @@ import devlight.io.library.ntb.NavigationTabBar;
 
 public class MainActivity extends AppCompatActivity /*implements Tab1.OnFragmentInteractionListener,Tab2.OnFragmentInteractionListener,Tab3.OnFragmentInteractionListener*/ {
 MyViewPager myViewPager;
-NavigationTabBar navigationTabBar;
-    private static int COUNT=3;
+TabLayout tabLayout;
+//NavigationTabBar navigationTabBar;
+    private static int COUNT=4;
 
     private ArrayList<NavigationTabBar.Model> models=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
         myViewPager=new MyViewPager(getBaseContext());
-        navigationTabBar=new NavigationTabBar(getBaseContext());
+        //navigationTabBar=new NavigationTabBar(getBaseContext());
         ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getSupportFragmentManager());
         myViewPager.setAdapter(viewPagerAdapter);
-        myViewPager.setPageTransformer(true, new AccordionTransformer());
-
-        models.add(
+        //myViewPager.setPageTransformer(true, new AccordionTransformer());
+        tabLayout.setupWithViewPager(myViewPager,true);
+        /*models.add(
                 new NavigationTabBar.Model.Builder(
                         ContextCompat.getDrawable(this,R.drawable.abc),
                         Color.rgb(7,94,84)
@@ -57,7 +58,7 @@ NavigationTabBar navigationTabBar;
                         .build()
         );
         navigationTabBar.setModels(models);
-        navigationTabBar.setViewPager(myViewPager);
+        navigationTabBar.setViewPager(myViewPager);*/
     }
     class ViewPagerAdapter extends FragmentPagerAdapter {
         private int selection = 0;
@@ -80,11 +81,29 @@ NavigationTabBar navigationTabBar;
                 else
                     return Fragment2.newInstance();
             }
-            else {
-                if (selection == position)
+            if (position == 2){
+                if(selection==position)
                     return Fragment3.newInstance();
                 else
                     return Fragment3.newInstance();
+            }
+
+            else {
+                if (selection == position)
+                    return Fragment4.newInstance();
+                else
+                    return Fragment4.newInstance();
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position){
+                case 0 : return "Consult";
+                case 1 : return "Chats";
+                case 2 : return "Peoples";
+                case 3 : return "Profile";
+                default: return "DUMMY";
             }
         }
 
